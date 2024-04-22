4dfce12a7ef586321fcf8256b592b00232649c75
damiaojr91 <damiao.dsj@gmail.com>
2024-04-22 19:50:58 -0300

    Adding list filtering

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,25 +1,46 @@
+import { useSearchParams } from 'react-router-dom';
 import { Card } from '../../components/common/Card';
 import { Header } from '../../components/common/Header';
-import { Grid } from '../../components/layout/Grid';
-import styles from './Pets.module.css';
-import { useQuery } from '@tanstack/react-query';
-import { getPets } from '../../services/pets/getPets';
-import { Skeleton } from '../../components/common/Skeleton';
 import { Pagination } from '../../components/common/Pagination';
-import { useSearchParams } from 'react-router-dom';
+import { Skeleton } from '../../components/common/Skeleton';
+import { Grid } from '../../components/layout/Grid';
+import { usePetList } from '../../hooks/usePetList';
+import styles from './Pets.module.css';
+import { Select } from '../../components/common/Select';
+import { Button, ButtonVariant } from '../../components/common/Button';
+import { filterColumns } from './Pets.constants';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { GetPetsRequest } from '../../interfaces/pet';
 
 export function Pets() {
+  const [isButtonEnabled, setIsButtonEnabled] = useState(false)
   const [searchParams, setSearchParams] = useSearchParams()
 
   const urlParams = {
     page: searchParams.get('page') ? Number(searchParams.get('page')) : 1,
+    type: searchParams.get('type') ?? '',
+    size: searchParams.get('size') ?? '',
+    gender: searchParams.get('gender') ?? '',
   }
 
-  const { data, isLoading } = useQuery({
-    queryKey: ['get-pets', urlParams],
-    queryFn: () => getPets(urlParams),
-    // staleTime: 100 //Defines time to next request
-  });
+  const { data, isLoading } =  usePetList(urlParams)
+
+  function checkButtonStatus (event: ChangeEvent<HTMLFormElement>){
+    const { type, size, gender } = getFormValue(event.target.form)
+
+    if (type !== urlParams.type || size !== urlParams.size || gender !== urlParams.gender){
+      setIsButtonEnabled(true)
+    } else {
+      setIsButtonEnabled(false)
+    }
+  }
+
+  // Example of useQuery:
+  // const { data, isLoading } = useQuery({
+  //   queryKey: ['get-pets', urlParams],
+  //   queryFn: () => getPets(urlParams),
+  //   // staleTime: 100 //Defines time to next request
+  // });
 
   function changePage(page: number) {
     // Update the param page from url
@@ -29,10 +50,53 @@ export function Pets() {
     })
   }
 
+  function getFormValue(form: HTMLFormElement) {
+    const formData = new FormData(form)
+    return Object.fromEntries(formData)
+  }
+
+  function updateSearchParams(urlParams: GetPetsRequest) {
+    const fields: (keyof GetPetsRequest)[] = ['type', 'size', 'gender']
+    const newParams = new URLSearchParams()
+
+    fields.forEach((field) => {
+      if (urlParams[field]) {
+        newParams.set(field, String(urlParams[field]))
+      }
+    })
+    newParams.set('page', '1')
+
+    return newParams
+  }
+
+  function applyFilters(event: FormEvent) {
+    event.preventDefault()
+
+    const formValues = getFormValue(event.target as HTMLFormElement)
+    const newSearchParams = updateSearchParams(formValues)
+
+    setSearchParams(newSearchParams)
+    setIsButtonEnabled(false)
+  }
+
   return (
     <Grid>
       <div className={styles.container}>
         <Header />
+        <form className={styles.filters} onSubmit={applyFilters} onChange={checkButtonStatus}>
+          <div className={styles.columns}>
+            {filterColumns.map((filter) => (
+              <div key={filter.name} className={styles.column}>
+                <Select
+                  label={filter.title}
+                  defaultValue={urlParams[filter.name]}
+                  name={filter.name}
+                  options={filter.options}/>
+              </div>
+            ))}
+          </div>
+          <Button type="submit" variant={ isButtonEnabled ? ButtonVariant.Default : ButtonVariant.Disabled }>Buscar</Button>
+        </form>
 
         {isLoading && <Skeleton containerClassName={styles.skeleton} count={10} />}
 
